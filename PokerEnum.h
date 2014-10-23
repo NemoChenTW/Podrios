f2e37b1c59f558a73a2beee31e7372a8d669525d
NemoChen <nemo7716@gmail.com>
2014-10-23 21:22:03 +0800

    Create PokerEnum.h and modify .gitignore.

---
+++ PokerEnum.h
@@ -0,0 +1,33 @@
+#ifndef __POKERENUM_H
+#define __POKERENUM_H
+
+enum PokerSuit
+{
+	PokerSuit_0,
+	PokerSuit_Hearts,
+	PokerSuit_Diamonds,
+	PokerSuit_Clubs,
+	PokerSuit_Spades,
+	PokerSuit_MAX
+}
+
+enum PokerPoint
+{
+	PokerPoint_0,
+	PokerPoint_A,
+	PokerPoint_1,
+	PokerPoint_2,
+	PokerPoint_3,
+	PokerPoint_4,
+	PokerPoint_5,
+	PokerPoint_6,
+	PokerPoint_7,
+	PokerPoint_8,
+	PokerPoint_9,
+	PokerPoint_10,
+	PokerPoint_J,
+	PokerPoint_Q,
+	PokerPoint_K,
+}
+
+#endif
