31d33460163f49239751bd1fa84568c456a6910a
NemoChen <nemo7716@gmail.com>
2014-10-25 11:03:17 +0800

    Create class CardDeck

---
+++ CardDeck.cpp
@@ -0,0 +1,20 @@
+/*
+ * CardDeck.cpp
+ *
+ *  Created on: 2014年10月25日
+ *      Author: nemo
+ */
+
+#include "CardDeck.h"
+
+CardDeck::CardDeck()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+CardDeck::~CardDeck()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ CardDeck.h
@@ -0,0 +1,21 @@
+/*
+ * CardDeck.h
+ *
+ *  Created on: 2014年10月25日
+ *      Author: nemo
+ */
+
+#ifndef CARDDECK_H_
+#define CARDDECK_H_
+
+#include "Card.h"
+
+class CardDeck {
+public:
+	CardDeck();
+	virtual ~CardDeck();
+
+
+};
+
+#endif /* CARDDECK_H_ */
